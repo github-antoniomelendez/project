e01d215d3f0a68a1313741be1cf765a5b58e97ea
Antonio Melendez <mel23001@byui.edu>
2025-08-01 20:13:53 -0600

    form

--- scripts/siteplan.js
+++ scripts/siteplan.js
@@ -1,4 +1,39 @@
-// Gets the current year and the last modification date
+// Populate Info Cards
+  const cards = [
+    { id: "1", name: "Leonardo"},
+    { id: "2", name: "Raphael"},
+    { id: "3", name: "Michelangelo"},
+    { id: "4", name: "Donatello"},
+    { id: "5", name: "Splinter"}
+  ];
+
+  document.addEventListener("DOMContentLoaded", () => {
+  const cardSelect = document.getElementById("card");
+
+  if (cardSelect) {
+    cards.forEach(card => {
+      const option = document.createElement("option");
+      option.value = card.id;
+      option.textContent = card.name;
+      cardSelect.appendChild(option);
+    });
+  }
+});
+
+// Increment localStorage counter
+  document.addEventListener("DOMContentLoaded", () => {
+  let count = localStorage.getItem("reviewCount");
+  count = count ? parseInt(count) : 0;
+  count++;
+  localStorage.setItem("reviewCount", count);
+  
+  const countDisplay = document.getElementById("review-count");
+  if (countDisplay) {
+    countDisplay.textContent = count;
+  }
+});
+
+// Get the current year and the last modification date
 document.addEventListener("DOMContentLoaded", () => {
     const year = new Date().getFullYear();
     document.getElementById("currentyear").textContent = year;
